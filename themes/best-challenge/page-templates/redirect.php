97df783059a167274002c6c2791f696063706a39
Brett MacDonald <macdonald.brett@outlook.com>
2016-11-29 13:45:10 -0800

    added styling to redirect page

--- themes/best-challenge/page-templates/redirect.php
+++ themes/best-challenge/page-templates/redirect.php
@@ -8,18 +8,28 @@
  * @package Best_Challenge_Theme
  */
 
-get_header(); ?>
-
+ ?>
+<?php get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+<div class="redirect-page-area">
+			<div class="page-banner">
+			<h1>hooray!!!</h1>
+			<h2>See you later!</h2>
+			</div>
 
-			<?php endwhile; // End of the loop. ?>
-test
+			<div class="redirect-explain">
+				<p>You are now leaving Commuter Challenge BC and will 
+					automatically be redirected to <span>Commuter Challenge - Canada to Register.</span>
+			</div>
+			<div class ="resources">
+				<p>Don't forget we have got your back with fun and unique <a href="">BC Resources</a></p>
+			</div>
+</div>
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-
+<?php get_footer(); ?>
