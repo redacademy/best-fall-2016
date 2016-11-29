ce88e375b42c5490bead24321e14d5b33ce1cb16
Anna Kviese <anna.kviese@gmail.com>
2016-11-29 09:14:11 -0800

    added gulpfile to gitignore

--- themes/best-challenge/gulpfile.js
+++ themes/best-challenge/gulpfile.js
@@ -63,7 +63,7 @@ gulp.task('browser-sync', function() {
    ];
 
     browserSync.init(files, {
-        proxy: 'inhabitent.dev',
+        proxy: 'localhost:8888/best',
     });
 
     gulp.watch(files).on('change', browserSync.reload);
