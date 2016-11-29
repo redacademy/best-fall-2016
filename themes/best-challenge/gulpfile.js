a0b10a91b7151073ec3553f90a3d364facb1af75
Brett MacDonald <macdonald.brett@outlook.com>
2016-11-29 09:18:32 -0800

    Removing ignored files

--- themes/best-challenge/gulpfile.js
+++ themes/best-challenge/gulpfile.js
@@ -63,7 +63,7 @@ gulp.task('browser-sync', function() {
    ];
 
     browserSync.init(files, {
-        proxy: 'inhabitent.dev',
+        proxy: 'localhost/wordpress_BEST',
     });
 
     gulp.watch(files).on('change', browserSync.reload);
