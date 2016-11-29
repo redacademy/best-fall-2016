8f8ef5bf1a56f30ba7a6413c4b10a67f032af5d1
Brandon Izzard <xbrandoni@gmail.com>
2016-11-29 11:15:35 -0800

    added header stuff

--- themes/best-challenge/header.php
+++ themes/best-challenge/header.php
@@ -15,8 +15,6 @@
 		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
                 rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1"
                 crossorigin="anonymous">
-				<link rel="stylesheet" type="text/css"
-          href="https://fonts.googleapis.com/css?family=Roboto">
 
 	<?php wp_head(); ?>
 	</head>
@@ -24,17 +22,26 @@
 	<body <?php body_class(); ?>>
 		<div id="page" class="hfeed site">
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
-
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
+		<div class="logo">
+			<a href="home">
+				<img src="http://i.imgur.com/B3ytW6K.png" alt="logo">
+			</a>
+		</div>
+		<div class="event-date">
+		<h2><span>June<span> 4 to 10, 2017</h2>
+		</div>
 
+		<div class="header-nav">
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav><!-- #site-navigation -->
+		</div>
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
