42e0ce769737fd4f502cfa9c7d351f772510d239
Brett MacDonald <macdonald.brett@outlook.com>
2016-11-28 16:55:50 -0800

    added content to footer

--- themes/best-challenge/footer.php
+++ themes/best-challenge/footer.php
@@ -11,7 +11,33 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+					<nav class="footer-nav">
+<div class="footer-column">
+	
+<div class="footer-sections">
+
+<p><span>b.e.s.t<span>is a non-profit charitable organization with a long history of
+	promoting sustainable transportation</p>
+<p class="section-heading"><a href="">learn more ></a></p>
+<p class="section-heading">contact us</p>
+</p>Do you have any questions? Contact us today!</p>
+
+<p class="section-heading"><a href="">commuterchallenge@best.bc.ca</a></p>
+<p>(604)669-2860</p>
+
+<li>
+<li>
+<li>
+</ul>
+
+
+
+
+</div>
+<div class="footer-different-color">
+<div class="mobile-column">
+</nav>
+					
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
@@ -20,3 +46,7 @@
 
 	</body>
 </html>
+
+
+
+
