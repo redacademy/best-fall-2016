b248ed4e8afb01d7e5d6396dce6a62af3195a7e2
Brandon Izzard <xbrandoni@gmail.com>
2016-11-29 11:51:22 -0800

    404 page

--- themes/best-challenge/404.php
+++ themes/best-challenge/404.php
@@ -18,34 +18,13 @@ get_header(); ?>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php echo esc_html( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?' ); ?></p>
-
-					<?php get_search_form(); ?>
-
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-					<?php if ( best_challenge_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php echo esc_html( 'Most Used Categories' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
-
-					<?php
-						$archive_content = '<p>' . sprintf( esc_html( 'Try looking in the monthly archives. %1$s' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
+					<div class="clock">
+						<img src="http://i1.istockimg.com/file_thumbview_approve/28439520/5/stock-illustration-28439520-digital-alarm-clock-cartoon.jpg" alt="clock">
+					</div>
+					<div>
+						<p> Looks like we hit the snooze button too many times and are late to work!</p>
+						<p><a href="#">Go back</a> or visit our <a href="#">Home Page</a></p>
+					</div>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
