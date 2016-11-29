731e4bae520c694556672f45c51417fae496891d
Brett MacDonald <macdonald.brett@outlook.com>
2016-11-29 10:48:37 -0800

    styled footer

--- themes/best-challenge/footer.php
+++ themes/best-challenge/footer.php
@@ -14,21 +14,29 @@
 			<div class="footer-column">
 				<div class="footer-section">
 					<div class="footer-content">
+						<div class="logo-image">
+							<i class="fa fa-bicycle" aria-hidden="true"></i>
+						</div>
 						<div class="about-best">
 							<p class="para-heading">About<span> b.e.s.t</span></p>
-							<p><span>b.e.s.t<span> is a non-profit charitable organization with a long history of
+							<p><span>b.e.s.t</span> is a non-profit charitable organization with a long history of
 							promoting sustainable transportation</p>
-							<p class="para-heading"><a href="">learn more ></a></p>
 						</div>
+						<div class="learn-more">
+							<p class="para-heading"><a href="">learn more ></a></p>
+							</div>
 						<div class="contact-us">
 							<p class="para-heading">contact us</p>
-							<p>Do you have any questions? Contact us today!</p>
-							<p class="para-heading"><a href="">commuterchallenge@best.bc.ca</a></p>
+						<p>Do you have any questions? Contact us today!</p>
+						</div>
+						<div class="questions">
+							<p><a href="">commuterchallenge@best.bc.ca</a></p>
 							<p>(604)669-2860</p>
 						</div>
 						<div class="newsletter-subscription">
 							<p class="para-heading">newsletter subscription</p>
 							<p>hear about upcoming events, new tools, and lots more!</p>
+							</div>
 							<div class="subscribe">
             					<div class="subscribeForm">
                 					<form id="formId">
@@ -39,7 +47,6 @@
         					</div>
 						</div>
 					</div>
-				</div>
 			<div class="footer-social">
 					<span><i class="fa fa-facebook-square"></i></span>
 					<span><i class="fa fa-instagram"></i></span>
--- themes/best-challenge/header.php
+++ themes/best-challenge/header.php
@@ -15,6 +15,8 @@
 		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
                 rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1"
                 crossorigin="anonymous">
+				<link rel="stylesheet" type="text/css"
+          href="https://fonts.googleapis.com/css?family=Roboto">
 
 	<?php wp_head(); ?>
 	</head>
