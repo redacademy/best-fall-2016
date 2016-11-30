1937ed58eed1bd2e32c3fbfc0d9df5bc9cc15c8f
Brett MacDonald <macdonald.brett@outlook.com>
2016-11-29 16:21:11 -0800

    styled get_involved

--- themes/best-challenge/page-templates/get_involved.php
+++ themes/best-challenge/page-templates/get_involved.php
@@ -14,37 +14,68 @@ get_header(); ?>
                     <h1>get your workplace involved!</h1>
                 </div>
 
-                <div class="commuter-challenge-benefits-wrapper">
-                    
-                    
-                    <div class="">
+                <div class="challenge-benefit-blocks">
+                    <div class="single-benefit-block">
                         <h3>Increased Productivity</h3>
-                    <img src=></img>
+                    <img></img>
                     <p>Being multi-modal reduces commute-related stress and increases productivity and morale</p>
                     </div>
-                    <div class="">
-                        <h3>Healthier LifeStyle</h3>
-                    <img scr=></img>
+                    <div class="single-benefit-block">
+                        <h3>Healthier Lifestyle</h3>
+                    <img></img>
                     <p>Active and sustainable commute enhances your physical, mental, and emotional health</p>
                     </div>
-                    <div class="">
+                    <div class="single-benefit-block">
                         <h3>Reduced Cost</h3>
-                    <img src=""></img>
+                   <img></img>
                     <p>Being multi-modal reduces commuting and facility related costs, benefiting employees and workplaces</p>
                     </div>
-
-
                 </div>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+                <div class="how-to-participate-wrapper">
+                    <h2>how to particpate as a workforce</h2>
+                <div class="how-to-participate">
+                     <div class="challenge-benefit-blocks">
+                    <div class="single-benefit-block">
+                        <h3>Sing up to participate</h3>
+                    <img></img>
+                    <p>Being multi-modal reduces commute-related stress and increases productivity and morale</p>
+                    </div>
+                    <div class="single-benefit-block">
+                        <h3>Promote and Engage Team</h3>
+                    <img></img>
+                    <p>Active and sustainable commute enhances your physical, mental, and emotional health</p>
+                    </div>
+                    <div class="single-benefit-block">
+                        <h3>Track and Compete</h3>
+                   <img></img>
+                    <p>Being multi-modal reduces commuting and facility related costs, benefiting employees and workplaces</p>
+                    </div>
+                    <div class="single-benefit-block">
+                    <h3>Win Prizes and Party</h3>
+                   <img></img>
+                    <p>Being multi-modal reduces commuting and facility related costs, benefiting employees and workplaces</p>
+                    </div>
+                </div>
+            </div>
+            	<div class="signUpForm">
+                	<form id="sign-up-form">
+						<input type="submit" name="subscribe" value="Sign Up" id="subscribe">
+                        <p>on Canada Site</p>
+                        <p>or log in on the canada site</p>
+                	</form>
+            	</div>
+
+
+			<!--<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
-            </div>
+            </div>-->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
