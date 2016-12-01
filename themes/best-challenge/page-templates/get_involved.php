b54b7eb403130ee08f8257be1a219d568cafe3a6
Brett MacDonald <macdonald.brett@outlook.com>
2016-12-01 09:00:05 -0800

    adding partial

--- themes/best-challenge/page-templates/get_involved.php
+++ themes/best-challenge/page-templates/get_involved.php
@@ -7,6 +7,14 @@
 
 get_header(); ?>
 
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php endwhile; // End of the loop. ?>
+            </div>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
             <div class="content-area-mobile">
@@ -32,7 +40,7 @@ get_header(); ?>
                     </div>
                 </div>
 
-                <!-- This is the code for the custom fields -->
+     <!-- This is the code for the custom fields -->
 
             
                 <div class="how-to-participate-wrapper">
@@ -57,17 +65,18 @@ get_header(); ?>
                                         echo $field['header'];?></h3>
                                         
                                         <div class="description-wrapper">
-                                        <p><?php 
-                                        echo $field['description'];?></p>
+                                            <p><?php 
+                                            echo $field['description'];?></p>
                                         </div>
-    
-                                </div>
+                                 </div>
                             <?php endforeach; ?>
+    <!-- This is the end of the custom field code -->
                         </div>
                      </div>
 
                     <div class="signUpForm">
                 	    <form id="sign-up-form">
+                             <!-- This link will take user to extrnal site -->
 						    <input type="submit" name="subscribe" value="Sign Up" id="sign-up">
                             <div class="font-under-button"><p>on Canada Site</p></div>
                             <div class="underline-under-button"><p>or log in on the canada site</p><div>
@@ -77,49 +86,52 @@ get_header(); ?>
             </div>
         </div>
 
-<!-- This is the end of the custom field code -->
-		
-             <div class="resources-section">
-
-                 <div class="need-resources">
+            <div class="resources-section">
+                <div class="need-resources">
                         <img src="http://images.all-free-download.com/images/graphiclarge/yin_yang_symbol_black_round_sticker_311430.jpg"/>
-                    
                         <h2>need resources?</h2>
                         <p>Check out our resources page for everything you need to know about engaging your coworkers and 
                             promoting the event (email templates, posters and more!)
                         </p>
-                        <div class="resources-link">
+                    <div class="resources-link">
                            <a href="">Check out resources ></a>
-                        </div>
-                        <div class="need-prizes">
+                            <!-- This link will take user to another page for more resources -->
+                    </div>
+                    <div class="need-prizes">
                              <img src="http://images.all-free-download.com/images/graphiclarge/yin_yang_symbol_black_round_sticker_311430.jpg"/>
                         <h2>prizes for everyone</h2>
                         <p>We have prizes for everyone-those who are trying something new and those who are already commuting 
                             sustainably, high-achievers, and lucky ones, teams and individuals. Check them out!
                         </p>
-                        <div class="resources-link">
+                    <div class="resources-link">
                             <a href="">Check out prizes ></a>
-                        </div>
+                             <!-- This link will take user prizes page -->
                     </div>
-                    </div>
-                    </div>
-                    <div class="testimonial">
-                        <p>"Several coworkers told me commuting by bike used to be a remote idea to them. They were glad to try it 
-                            during the Commuter Challenge"</p>
-                        <div class="testimonial-author">
+                </div>
+            </div>
+        </div>
+                <div class="testimonial">
+                        <p><em>"Several coworkers told me commuting by bike used to be a remote idea to them. They were glad to try it 
+                            during the Commuter Challenge"</em></p>
+                    <div class="testimonial-author">
                         <p>Allison W</p>
                         <p>Commuter Challenge Coordinator</p>
                     </div>
-                
-           
-
-
-			<!--<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
-            </div>
+                </div>
+                <!-- This is the beginning of a template part that toggles a button on and off -->
+                    <div class="green-banner">
+                        <div>
+                            <p>this is some text that will end up being very annoying I believe</p>
+                            <p>this is some other text that will be even more annoying</p>
+                        </div>
+                        <div class="button1">
+                            <input type="submit" name="subscribe" value="thisOneMayDisappear" id="go-green-button">
+                        </div>
+                        <div class="button2">
+                            <input type="submit" name="subscribe" value="thisOneMightToo" id="go-green-button">
+                        </div>
+                    </div>
+                <!-- This is the end of that template part  -->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
