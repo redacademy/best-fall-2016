35abacfccd83aae4f6d5acf951411d3919767c6a
Brett MacDonald <macdonald.brett@outlook.com>
2016-12-01 10:49:08 -0800

    added template part to page

--- themes/best-challenge/template-parts/green-banner.php
+++ themes/best-challenge/template-parts/green-banner.php
@@ -1,5 +1,14 @@
-<div class="green-banner">
-    
-
-
-</div>
+     <!-- This is the beginning of a template part that toggles a button on and off -->
+                    <div class="green-banner">
+                        <div>
+                            <p>this is some text that will end up being very annoying I believe</p>
+                            <p>this is some other text that will be even more annoying</p>
+                        </div>
+                        <div class="button1">
+                            <input type="submit" name="subscribe" value="thisOneMayDisappear" id="go-green-button">
+                        </div>
+                        <div class="button2">
+                            <input type="submit" name="subscribe" value="thisOneMightToo" id="go-green-button">
+                        </div>
+                    </div>
+                <!-- This is the end of that template part  -->
