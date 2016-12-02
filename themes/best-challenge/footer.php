123dccdcd93d7a37ce53fa0f6d99c8aa775f2591
Brett MacDonald <macdonald.brett@outlook.com>
2016-12-02 15:30:21 -0800

    styled footer

--- themes/best-challenge/footer.php
+++ themes/best-challenge/footer.php
@@ -19,7 +19,7 @@
 						</div>
 						<div class="about-best">
 							<div class="para-heading"><p>About<span> b.e.s.t</span></p></div>
-							<div class="para-content"><p><span>b.e.s.t</span> is a non-profit charitable organization with a long history of
+							<div class="para-content"><p>B.E.S.T is a non-profit charitable organization with a long history of
 							promoting sustainable transportation</p></div>
 						</div>
 						<div class="learn-more">
@@ -29,19 +29,23 @@
 							<div class="para-heading"><p>contact us</p><div>
 						<div class="para-content"><p>Do you have any questions? Contact us today!</p></div>
 						</div>
+						<div class="commuterChallenge-contact-logos">
+							<i class="fa fa-phone" aria-hidden="true"></i>
+							<i class="fa fa-envelope-o" aria-hidden="true"></i>
+						</div>
 						<div class="questions">
 							<p><a href="">commuterchallenge@best.bc.ca</a></p>
 							<div class="para-content"><p>(604)669-2860</p></div>
 						</div>
 						<div class="newsletter-subscription">
-							<div class="para-heading"><p>newsletter subscription</p></div>
-							<div class="para-content"><p>hear about upcoming events, new tools, and lots more!</p></div>
+							<div class="para-heading"><p>Newsletter Subscription</p></div>
+							<div class="para-content"><p>Hear about upcoming events, new tools, and lots more!</p></div>
 						</div>
-							<div class="subscribe">
-            					<div class="subscribeForm">
-                					<form id="formId">
-									<input type="email" name="your_email" id="your-email" value="" placeholder="Enter your email..." aria-label="Subscribe Email Input">
-                    				<input type="submit" name="subscribe" value="Subscribe" id="subscribe">
+							<div class="subscribe-to-newsletter">
+            					<div class="subscribeForm-for-newsLetter">
+                					<form id="newsLetter-formId">
+									<div class="sign-up-email"><input type="email" name="your_email" id="your-email" value="" placeholder="Enter your email..." aria-label="Subscribe Email Input"></div>
+                    				<div class="sign-up-button"><input type="submit" name="subscribe" value="Subscribe" id="subscribe"></div>
                 					</form>
             					</div>
         					</div>
