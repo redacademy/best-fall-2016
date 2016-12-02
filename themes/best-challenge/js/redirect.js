9d6cd660673e257eabbf9cb54ecd43081b5dea77
Brett MacDonald <macdonald.brett@outlook.com>
2016-12-02 10:53:30 -0800

    added functionality to template part

--- themes/best-challenge/js/redirect.js
+++ themes/best-challenge/js/redirect.js
@@ -9,13 +9,17 @@
 (function ($) {
     $('#sign-up').on("click", function (event) {
         event.preventDefault();
- $('.toggle-visible-hidden').remove("toggle-visible-hidden");
+        $( "div" ).first().remove();
+        $( "footer" ).remove();
+        $('.content-area').remove()
+    $('.toggle-visible-hidden').css("visibility", "visible");
+    $('.toggle-visible-hidden').css("height", "100vh");
 
 window.setTimeout(function () {
         location.href = "https://commuterchallenge.ca/";
     }, 5000)
 
- $('.toggle-visible-hidden').remove("toggle-visible-hidden");
+
       
     });
 
