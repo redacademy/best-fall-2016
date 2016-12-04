3e015e8ebcaa9caf1a6f024b64a2658869ab360c
Brett MacDonald <macdonald.brett@outlook.com>
2016-12-04 15:07:23 -0800

    made contact person a template part

---
+++ themes/best-challenge/template-parts/contact-person.php
@@ -0,0 +1,15 @@
+       <!-- This is the beginning of a template part that shows a picture and contact infor for contact person -->
+  
+  <div class="contact-person-section">
+                <div class="contact-person-image">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/images/stephen-white.png"></img>
+                </div>
+                <div class="contact-person-data">
+                    <h2>connect with us directly</h2>
+                    <h2>we are here to help!</h2>
+                    <p>Say 'Hi' to Stephen from B.E.S.T</p>
+                    <p>Stephen White | stephen@best.bc.ca | (604) 669-2860 ext.200</p>
+                </div>
+            </div>
+ <!-- This is the beginning of a template part that shows a picture and contact infor for contact person -->
+  
