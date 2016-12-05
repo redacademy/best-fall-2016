1311045f3eb70b8905e92e998befcf11896688eb
Brandon Izzard <xbrandoni@gmail.com>
2016-12-04 19:19:30 -0800

    logo fix

--- themes/best-challenge/header.php
+++ themes/best-challenge/header.php
@@ -35,7 +35,7 @@
 				<div class="logo">
 					<a href="home">
 						<img src=" <?php echo get_template_directory_uri() ?>/assets/images/logotitlebc-mobile.png" alt="logo" class= "mobile">
-						<img src=" <?php echo get_template_directory_uri() ?>/assets/images/logotitlebc.png" alt="logo" class= "desktop">
+						<img src=" <?php echo get_template_directory_uri() ?>/assets/images/bccommuterchallenge.png" alt="logo" class= "desktop">
 					</a>
 				</div>
 			<div class="event-date">
