20325a56fd9e503a58f8579d2ec8f1a848a403f6
Anna Kviese <anna.kviese@gmail.com>
2016-12-07 09:14:37 -0800

    added hierarchy to the activity taxonomy

--- plugins/best-functionality/lib/functions/taxonomies.php
+++ plugins/best-functionality/lib/functions/taxonomies.php
@@ -150,7 +150,7 @@ function register_activities_taxonomy_() {
 	);
 	$args = array(
 		'labels'                     => $labels,
-		'hierarchical'               => false,
+		'hierarchical'               => true,
 		'public'                     => true,
 		'show_ui'                    => true,
 		'show_admin_column'          => true,
