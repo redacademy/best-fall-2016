f6dd7938e6c6d42cfa2e2127dd80b6712ed60334
Brandon Izzard <xbrandoni@gmail.com>
2016-12-07 10:22:28 -0800

    impact results header and page made

--- themes/best-challenge/page-results.php
+++ themes/best-challenge/page-results.php
@@ -8,6 +8,17 @@
 get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<div class = "page-heading">	
+				 <div class="picture-wrapper-results">		
+					<?php the_title( '<div class="heading"><h1>', '</h1></div>' ); ?>
+					<p>British Columbia Commuter Challenge participants made a real impact!</p>
+            		<div class="title-info">
+						<?php echo CFS()->get( 'title_info'); ?>
+					</div>
+				</div>
+			</div>
+			<div class="content"> 
+				</div>
 			
 
 
