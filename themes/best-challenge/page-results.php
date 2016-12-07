d7d3ab7dee5b2359694e5a18b3fe4b62ba277c98
Brandon Izzard <xbrandoni@gmail.com>
2016-12-06 22:04:56 -0800

    create impact results page

---
+++ themes/best-challenge/page-results.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template for the Results page. 
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
