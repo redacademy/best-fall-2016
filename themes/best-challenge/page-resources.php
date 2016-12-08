4d8944f16da4327415b7801fa48d7138ea706a85
Anna Kviese <anna.kviese@gmail.com>
2016-12-07 20:24:22 -0800

    added a loop to display taxonomy posts and it's children in Resources

--- themes/best-challenge/page-resources.php
+++ themes/best-challenge/page-resources.php
@@ -29,47 +29,89 @@ get_header(); ?>
 			</div>
 			
 			<?php
-// List posts by the terms for a custom taxonomy 
-				$terms = get_terms('download_type', array('order' => 'DESC'));  
+
+	   // for every child of  show with sub-children show sub-children
+		// get all from main parent - download_type
+
+			$taxonomy = 'download_type';
+			$terms = get_terms($taxonomy, array('parent' => 0, 'order' => 'DESC'));   
 				foreach($terms as $download_type) { ?>
 					<div class="taxonomy-wrapper">
 						<div class="results">
-							<h2><?php echo $download_type->name; ?> </h2>
+							<h2><?php echo $download_type->name; ?> </h2> 
+						<?php 
 
-							<?php $args = array(
+						$children_ids = get_term_children( $download_type->term_id, $taxonomy );
+
+						if (count($children_ids)> 0) {					//if taxonomy has children, show it's children and posts
+							foreach( $children_ids as $child_id ) {
+								$child = get_term($child_id, $taxonomy); ?>
+									<h3><?php echo $child->name; ?> </h3> 
+									<?php
+										$args = array(
+											'post_type' => 'resources',
+											'posts_per_page' => 3,
+											'tax_query' => array( 
+												array(
+													'taxonomy' => $taxonomy,
+													'field'    => 'slug',
+													'terms'    =>  $child,										
+												),
+											),  
+										);
+										$downloads = get_posts( $args );
+
+								foreach( $downloads as $post ) : setup_postdata($post); ?>
+									<div class="taxonomy-post">
+										<div class="taxonomy-image">
+											<?php the_post_thumbnail( 'large' ); ?>							
+										</div>
+
+										<div class="taxonomy-title">
+											<h3><?php the_title(); ?></h3>
+										</div>
+										<div class="taxonomy-description">
+											<p><?php the_content(); ?></p>
+										</div>
+									</div>
+								<?php endforeach; wp_reset_postdata();
+							}
+						} 
+						else {	// show me the posts 
+							$args = array(
 								'post_type' => 'resources',
-								// 'posts_per_page' => 4,
+								'posts_per_page' => 3,
 								'tax_query' => array( 
 									array(
-										'taxonomy' => 'download_type',
+										'taxonomy' => $taxonomy,
 										'field'    => 'slug',
 										'terms'    =>  $download_type,
 										
 									),
 								),  
-							);	
-							$downloads = get_posts( $args ); ?>
-						
-						
-							<?php foreach( $downloads as $post ) : setup_postdata($post); ?>
-								<div class="taxonomy-post">
-									<div class="prize-image">
-										<?php the_post_thumbnail( 'large' ); ?>							
-									</div>
-
-									<div class="taxonomy-title">
-										<h3><?php the_title(); ?></h3>
-									</div>
-									<div class="taxonomy-description">
-										<p><?php the_content(); ?></p>
-									</div>
+							);
+						$downloads = get_posts( $args ); 
+						?>
+						<?php foreach( $downloads as $post ) : setup_postdata($post); ?>
+							<div class="taxonomy-post">
+								<div class="prize-image">
+									<?php the_post_thumbnail( 'large' ); ?>							
 								</div>
-							<?php endforeach; wp_reset_postdata(); ?>
-						</div>
-					</div> <!-- prize wrapper section ends -->
-				<?php
-				} ?>
 
+								<div class="taxonomy-title">
+									<h3><?php the_title(); ?></h3>
+								</div>
+								<div class="taxonomy-description">
+									<p><?php the_content(); ?></p>
+								</div>
+							</div>
+						<?php endforeach; wp_reset_postdata(); ?>
+					</div>
+				</div> <!-- prize wrapper section ends -->
+			<?php
+				} 
+			}
+			?>
 		</div>
 	</div><!-- #primary -->
 
