46c9e03fe9e3a8cab16bf715883d4a04124bba90
Brandon Izzard <xbrandoni@gmail.com>
2016-12-08 09:32:16 -0800

    seperating typewriter function

---
+++ themes/best-challenge/js/typewriter.js
@@ -0,0 +1,58 @@
+/**
+ * typewriter.js
+ *
+ * Helps with accessibility for keyboard only users.
+ *
+ * Learn more: https://github.com/Automattic/RED Starter Theme/pull/136
+ */
+// Typewriter Effect 
+
+document.addEventListener('DOMContentLoaded',function(event){
+  // array with texts to type in typewriter
+  var dataText = [ "Car Pool.", "Bicycle.", "Bus.", "Train.", "Foot.", "Roller-blades."];
+  
+  // type one text in the typwriter
+  // keeps calling itself until the text is finished
+  function typeWriter(text, i, fnCallback) {
+    // chekc if text isn't finished yet
+    if (i < (text.length)) {
+      // add next character to h1
+     document.getElementById("typewrite").innerHTML = text.substring(0, i+1);
+
+      // wait for a while and call this function again for next character
+      setTimeout(function() {
+        typeWriter(text, i + 1, fnCallback)
+      }, 150);
+    }
+    // text finished, call callback if there is a callback function
+    else if (typeof fnCallback == 'function') {
+      // call callback after timeout
+      setTimeout(fnCallback, 700);
+    }
+  }
+  // start a typewriter animation for a text in the dataText array
+   function StartTextAnimation(i) {
+     if (typeof dataText[i] == 'undefined'){
+        setTimeout(function() {
+          StartTextAnimation(0);
+        }, 5000);
+     }
+     // check if dataText[i] exists
+     if (dataText[i] == null){
+       
+     }
+     else{
+    if (i < dataText[i].length) {
+      // text exists! start typewriter animation
+     typeWriter(dataText[i], 0, function(){
+       // after callback (and whole text has been animated), start next text
+       StartTextAnimation(i + 1);
+     });
+    }
+   }
+  }
+  // start the text animation
+  StartTextAnimation(0);
+});
+
+// Typewriter end
