e80e413ac079135690a5de6f9e735cfeb765ee89
Brett MacDonald <macdonald.brett@outlook.com>
2016-12-07 20:04:55 -0800

    cleaned up code

--- themes/best-challenge/template-parts/redirect.php
+++ themes/best-challenge/template-parts/redirect.php
@@ -1,10 +1,9 @@
 
-<div class="redirect-page-area">
+		<div class="redirect-page-area">
 			<div class="page-banner">
-			<h1>hooray!!!</h1>
-			<h2>See you later!</h2>
+				<h1>hooray!!!</h1>
+				<h2>See you later!</h2>
 			</div>
-
 			<div class="redirect-explain">
 				<p>You are now leaving Commuter Challenge BC and will 
 					automatically be redirected to <span>Commuter Challenge - Canada to Register.</span>
@@ -12,4 +11,4 @@
 			<div class ="resources">
 				<p>Don't forget we have got your back with fun and unique <a href="">BC Resources</a></p>
 			</div>
-</div>
+		</div>
