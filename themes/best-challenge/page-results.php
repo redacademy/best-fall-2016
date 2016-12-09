e2b320a81347b196194dd0c6d218ddb3242b0a52
Brandon Izzard <xbrandoni@gmail.com>
2016-12-08 16:04:36 -0800

    styling

--- themes/best-challenge/page-results.php
+++ themes/best-challenge/page-results.php
@@ -102,32 +102,7 @@ get_header(); ?>
 
 		
 			
-
-<div class="front-sponsors">
-				<div class="fp-sponsors-img container">
-				<div>
-					<img src=" <?php echo get_template_directory_uri() ?>/assets/images/evo-logo.png" alt="logo" class="desktop">
-				</div>
-				<div>
-					<img src=" <?php echo get_template_directory_uri() ?>/assets/images/movcolorstack-300.png" alt="logo" class="desktop">
-				</div>
-				<div>
-					<img src=" <?php echo get_template_directory_uri() ?>/assets/images/bc-logo.png" alt="logo">
-				</div>
-				<div class="modologo">
-					<img src=" <?php echo get_template_directory_uri() ?>/assets/images/modo-logo.png" alt="logo">
-				</div>
-				<div>
-					<img src=" <?php echo get_template_directory_uri() ?>/assets/images/century-group-logo.png" alt="logo">
-				</div>
-				<div>
-					<img src=" <?php echo get_template_directory_uri() ?>/assets/images/goodlife-fitness-gym-logo.png" alt="logo" class="desktop">
-				</div>
-				<div>
-					<img src=" <?php echo get_template_directory_uri() ?>/assets/images/mec-rgb-trademark.png" alt="logo" class="desktop">
-				</div>
-			</div>
-		</div>
+				
 
 		</div>
   <!-- This is the beginning of a template part that toggles a button on and off -->
@@ -153,9 +128,6 @@ get_header(); ?>
                    
   <!-- sponsors -->
                     	<div class="front-sponsors">
-				<div class="fp-sponsors-title container">
-					<h2 class="uppercase">Sponsored by</h2>
-				</div>
 				<div class="fp-sponsors-img container">
 				<div>
 					<img src=" <?php echo get_template_directory_uri() ?>/assets/images/evo-logo.png" alt="logo">
