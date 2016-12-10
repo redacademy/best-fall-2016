922da666ab39d90f853bed365d502e72d44e16b3
Brett MacDonald <macdonald.brett@outlook.com>
2016-12-10 12:04:54 -0800

    moved logo in footer

--- themes/best-challenge/footer.php
+++ themes/best-challenge/footer.php
@@ -15,10 +15,11 @@
 				<div class="footer-section">
 					<div class="footer-content">
 						<div class="first-column">
-							<div class="logo-image">
+							<div class="about-best">
+								<div class="logo-image">
 								<img src="<?php echo get_template_directory_uri() ?>/assets/images/best-logo.png"></img>
 							</div>
-							<div class="about-best">
+
 								<div class="para-heading"><p>About<span> b.e.s.t</span></p>
 								</div>
 								<div class="para-content"><p>B.E.S.T is a non-profit charitable organization with a long history of
@@ -39,7 +40,7 @@
 							<i class="fa fa-envelope-o" aria-hidden="true"></i>
 						</div>
 						<div class="questions">
-							<p><a href="mailto:commuterchallenge@best.bc.ca">commuterchallenge@best.bc.ca</a></p>
+							<p><a href="mailto:commuterchallenge@best.bc.ca">commuterchallenge@<br>best.bc.ca</a></p>
 							<div class="para-content"><p>(604)669-2860</p></div>
 						</div>
 					</div>	
