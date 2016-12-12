02b2d97159d3d296823c29ffd7db1a355e1dc9ca
Anna Kviese <anna.kviese@gmail.com>
2016-12-12 15:51:11 -0800

    fixed the wp_footer

--- themes/best-challenge/functions.php
+++ themes/best-challenge/functions.php
@@ -95,18 +95,18 @@ function best_challenge_scripts() {
 	
 	wp_enqueue_script( 'jquery' );
 
-	wp_enqueue_script( 'redirect', get_template_directory_uri() . '/build/js/redirect.min.js', array ( 'jquery' ), 1.1, true);
+	wp_enqueue_script( 'redirect', get_template_directory_uri() . '/build/js/redirect.min.js', array( 'jquery' ), 1.1, true);
 
-	wp_enqueue_script( 'green-button-redirect', get_template_directory_uri() . '/build/js/green-button-redirect.min.js', array ( 'jquery' ), 1.1, true);
+	wp_enqueue_script( 'green-button-redirect', get_template_directory_uri() . '/build/js/green-button-redirect.min.js', array( 'jquery' ), 1.1, true);
 
 
-	wp_enqueue_script( 'dropdown', get_template_directory_uri() . '/build/js/dropdown.min.js', array ( 'jquery' ), 1.1, true);
+	wp_enqueue_script( 'dropdown', get_template_directory_uri() . '/build/js/dropdown.min.js', array( 'jquery' ), 1.1, true);
 
-	wp_enqueue_script ( 'scripts', get_template_directory_uri() . '/build/js/scripts.min.js', array ( 'jquery' ), false, true);
+	wp_enqueue_script ( 'scripts', get_template_directory_uri() . '/build/js/scripts.min.js', array( 'jquery' ), false, true);
 
 	if( is_front_page() )
     {
-       wp_enqueue_script( 'typewriter', get_template_directory_uri() . '/build/js/typewriter.min.js', array ( 'jquery' ), false, true);
+       wp_enqueue_script( 'typewriter', get_template_directory_uri() . '/build/js/typewriter.min.js', array( 'jquery' ), false, true);
     }
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
